567558190e89f11a951cb23cd6e91bbeb4efd245
ImGreatest <cmayll@mail.ru>
2023-12-18 15:14:29 +0300

    add component for page of recover user profile

---
+++ src/app/recover/recover.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recover',
+  standalone: true,
+  imports: [],
+  templateUrl: './recover.component.html',
+  styleUrl: './recover.component.less'
+})
+export class RecoverComponent {
+
+}
